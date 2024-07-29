1622d4603481249a7f33d2cbe84abba399f3e1e4
BincyAnuz <153153364+BincyAnuz@users.noreply.github.com>
2024-07-29 21:03:15 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+// const heading = React.createElement("h1",{id: "heading"},"Hello World from React!");
+//  const root = ReactDOM.createRoot(document.getElementById("root"));
+//  root.render(heading)
+
+const parent = React.createElement("div",{id:"parent"}, [ React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"I am a h1 tag inside parent inside child"),React.createElement("h1",{id:"heading"},"I am a h1 tag who is a sibling of the above")]), React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"I am a h1 tag inside parent inside child"),React.createElement("h1",{id:"heading"},"I am a h1 tag who is a sibling of the above")])]
+ 
+    
+  )
+
+  console.log(parent);
+
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+  root.render(parent);
